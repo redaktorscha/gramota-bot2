90036ac3d2f87964775ef2478041d22d63a43991
Masha <nasonkina@yandex.ru>
2023-10-25 01:51:22 +0300

    add iconv-lite for decoding fetch results

--- src/bot/initServer.ts
+++ src/bot/initServer.ts
@@ -1,6 +1,9 @@
 //@ts-check
 
 import express, { Request, Response, NextFunction } from 'express';
+import { writeFile } from 'node:fs/promises';
+import iconv from 'iconv-lite';
+import { createReadStream } from 'node:fs';
 
 const STATUS_CODES = {
   ok: 200,
@@ -14,13 +17,16 @@ const PORT = Number(process.env.PORT) || 3000;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const SECRET_ROUTE = `/${BOT_TOKEN}/`;
 
-const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   res
     .status(STATUS_CODES.errorServer)
     .send(`Oops, something went wrong! ${error.message}`);
 };
-
-
 
 const initServer = () => {
   const app = express();
@@ -37,10 +43,44 @@ const initServer = () => {
         .send('<h1>Server up!<h1>');
     })
 
-    .post(SECRET_ROUTE, (req: Request, res: Response) => {
+    .post(SECRET_ROUTE, async (req: Request, res: Response) => {
       const { message } = req.body;
       if (message) {
-        console.log('got new message: ', message); // here goes word check
+        console.log('got new message: ', message);
+        const queryUrl = `${process.env.DICT_URI}${message}`;
+        const response = await fetch(queryUrl);
+        if (!response.ok) {
+          console.log('Gramota Error');
+        } else {
+          response
+            .arrayBuffer()
+            .then((arrayBuffer) =>
+              iconv.decode(Buffer.from(arrayBuffer), 'win1251').toString()
+            )
+            .then((convertedString) => {
+              const substr = 'style="padding-left:50px">';
+              const closingTag = '</div';
+              const targetIndex = convertedString.indexOf(substr);
+              const cutIndexLeft = targetIndex + substr.length;
+
+              let queryResult = convertedString.slice(cutIndexLeft);
+              const cutIndexRight = queryResult.indexOf(closingTag);
+              queryResult = queryResult.slice(0, cutIndexRight);
+              console.log('queryResult', queryResult);
+              writeFile('./gramota-cut', queryResult);
+            });
+          // const substr = 'style="padding-left:50px">';
+          // const closingTag = '</div';
+          // const targetIndex = queryResult.indexOf(substr);
+          // const cutIndexLeft = targetIndex + substr.length;
+          // const cutIndexRight = queryResult.indexOf(closingTag);
+          // queryResult = queryResult.slice(cutIndexLeft).slice(0, cutIndexRight);
+          // console.log('queryResult', queryResult);
+          // const buffer = iconv.encode(queryResult, 'win1251');
+          // const decodedString = iconv.decode(buffer, 'utf8');
+          // console.log('decodedString', decodedString);
+          //writeFileSync('./gramota-res', queryResult);
+        }
       }
 
       res

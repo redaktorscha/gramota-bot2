f23e3915de40275f78691fb9fe979c888168e131
Masha <nasonkina@yandex.ru>
2023-11-09 01:40:02 +0300

    fix parser (chores)

--- src/parser.ts
+++ src/parser.ts
@@ -3,10 +3,7 @@ import { regexps } from './utils/regexps';
 import { readFileSync } from 'node:fs';
 import path from 'path';
 
-/**
- * arr of Russian letters that could have accent marks
- */
-const ACCENTS = [
+const ACCENTCHARS = [
   'а',
   'е',
   'и',
@@ -27,24 +24,16 @@ const ACCENTS = [
   'Я',
 ] as const;
 
-type AccentLetterType = (typeof ACCENTS)[number];
-
-// const isInReadonlyArray = <T, A extends T>(
-//   item: T,
-//   array: ReadonlyArray<A>
-// ): item is A => array.includes(item as A);
+type AccentLetterType = (typeof ACCENTCHARS)[number];
 
 const isAccentedLetter = (letter: string): letter is AccentLetterType =>
-  ACCENTS.includes(letter as AccentLetterType);
+ACCENTCHARS.includes(letter as AccentLetterType);
 
 const removeUnsupportedTags = (html: string) => {
   const { unsupportedHtmlTags } = regexps;
   return html.replace(unsupportedHtmlTags, '');
 };
 
-/**
- * text that could be found at the dict page
- */
 enum pageTargets {
   OpeningTagDiv = '<div style="padding-left:50px">',
   OpeningTagP = '<p style="padding-left:50px">',
@@ -57,39 +46,46 @@ enum pageTargets {
   SupTag = '<sup>',
 }
 
-const Sups = ['\u2070', '\u00B9', '\u00B2', '\u00B3', '\u2074', '\u2075', '\u2076', '\u2077', '\u2078', '\u2079'] as const;
+const Sups = [
+  '\u2070',
+  '\u00B9',
+  '\u00B2',
+  '\u00B3',
+  '\u2074',
+  '\u2075',
+  '\u2076',
+  '\u2077',
+  '\u2078',
+  '\u2079',
+] as const;
 
 const handleSups = (str: string) => {
-
   const isSupIndex = (s: string) => Number(s) < Sups.length && Number(s) >= 0;
 
   return str
-  .split(pageTargets.SupTag)
+    .split(pageTargets.SupTag)
 
-  .map((el) => {
-    if (isSupIndex(el[0])) {
-      return `${Sups[Number(el[0])]}${el.slice(1)}`;
-    }
-    return el;
-  })
-  .join('');
-}
+    .map((el) => {
+      if (isSupIndex(el[0])) {
+        return `${Sups[Number(el[0])]}${el.slice(1)}`;
+      }
+      return el;
+    })
+    .join('');
+};
 
 const cutAnswerString = (
   htmlString: string,
   openTag: string,
   closingTag: string
 ) => {
-  const tagLen = openTag.length
+  const tagLen = openTag.length;
   const leftSliceIndex = htmlString.indexOf(openTag) + tagLen;
   let resultString = htmlString.slice(leftSliceIndex);
   const rightSliceIndex = resultString.indexOf(closingTag);
   return resultString.slice(0, rightSliceIndex);
 };
 
-/**
- * cuts <br><br> that appears at the end of the string and inserts '/n' if the string contains more than one word
- */
 
 const insertLineBreaks = (str: string) => {
   const hasLineBreaksAtTheEnd = (s: string) =>
@@ -140,7 +136,11 @@ const parse = (html: string) => {
 
   let answerString = isNotExactMatch
     ? cutAnswerString(html, pageTargets.OpeningTagP, pageTargets.ClosingTagP)
-    : cutAnswerString(html, pageTargets.OpeningTagDiv, pageTargets.ClosingTagDiv);
+    : cutAnswerString(
+        html,
+        pageTargets.OpeningTagDiv,
+        pageTargets.ClosingTagDiv
+      );
 
   if (answerString.includes(pageTargets.LineBreaks)) {
     answerString = insertLineBreaks(answerString);
@@ -151,21 +151,15 @@ const parse = (html: string) => {
   }
 
   if (answerString.includes(pageTargets.AccentClass)) {
-    answerString = insertAccents(answerString)
+    answerString = insertAccents(answerString);
   }
 
-  
-    return removeUnsupportedTags(answerString);
+  answerString = removeUnsupportedTags(answerString);
 
-  // let leftSliceIndex = html.indexOf(pageTargets.OpeningTagDiv)
-  // const targetIndex = convertedString.indexOf(substr);
-  // const cutIndexLeft = targetIndex + substr.length;
-
-  // let queryResult = convertedString.slice(cutIndexLeft);
-  // const cutIndexRight = queryResult.indexOf(closingTag);
-  // queryResult = queryResult.slice(0, cutIndexRight);
-  // console.log('queryResult', queryResult);
-  // return queryResult;
+  if (isNotExactMatch) {
+    return `${botReplies.found_similar}${answerString}`;
+  }
+  return answerString;
 };
 
 // export default parse;

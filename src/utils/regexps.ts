74fd00b565f0b51e5c0ee9eba960bc16cea40ad8
Masha <nasonkina@yandex.ru>
2023-11-10 16:59:01 +0300

    add regex for replacing html entities

--- src/utils/regexps.ts
+++ src/utils/regexps.ts
@@ -2,5 +2,7 @@ export const regexps = {
     containsCyrillic: /\p{sc=Cyrillic}/u,
     onlyAsteriks: /^\*+$/,
     unsupportedHtmlTags: /<(?!\/?(b(?!r)|i|B|I)\b)[^>]*>/g,
+    htmlTags: /<[^>]*>/g,
     allowedChars: /[а-яё*?]+/ig,
+    htmlEntities: /(<(b|i)>)?&.*;(<\/(b|i)>)?/g,
 }
